9c152876b3db07396a5a26d4e2a820a6fb496edb
Mahmudul Hasan Autul <autul.aust@gmail.com>
2024-06-21 23:54:42 +0600

    Time: 120 ms (51.33%), Space: 54.2 MB (99.08%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,38 @@
+type symbolicValueObject = {
+  I: number;
+  V: number;
+  X: number;
+  L: number;
+  C: number;
+  D: number;
+  M: number;
+};
+
+const symbolicValue: symbolicValueObject = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  const stringList = s.split("");
+  let output = 0;
+  let index = 0;
+  while (index < stringList.length) {
+    if (
+      symbolicValue[stringList[index]] < symbolicValue[stringList[index + 1]]
+    ) {
+      output +=
+        symbolicValue[stringList[index + 1]] - symbolicValue[stringList[index]];
+      index++;
+    } else {
+      output += symbolicValue[stringList[index]];
+    }
+    index++;
+  }
+  return output;
+}
