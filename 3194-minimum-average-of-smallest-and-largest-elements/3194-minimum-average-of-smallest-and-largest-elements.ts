75976c4b4b371d4753b09a0936cf80728a25086d
Mahmudul Hasan Autul <autul.aust@gmail.com>
2024-06-26 23:28:53 +0600

    Time: 75 ms (20.41%), Space: 52.6 MB (10.20%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.ts
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.ts
@@ -5,14 +5,11 @@ function minimumAverage(nums: number[]): number {
     for(let i = 0; i < n/2; i++) {
         let minimum = nums[0]+nums[nums.length - 1];
         let minimumbytwo = minimum/2;
-        console.log(minimumbytwo)
         averages.push(minimumbytwo);
         nums.shift();
         nums.pop();
-        console.log(nums);
     }
     averages.sort((a,b)=> a-b);
     let minimumofavg = averages[0];
-    console.log(averages);
     return minimumofavg;
 };
