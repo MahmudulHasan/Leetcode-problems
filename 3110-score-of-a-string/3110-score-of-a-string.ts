07f491f7eeb9acba28d086276e9608e8b530e0a8
Mahmudul Hasan Autul <autul.aust@gmail.com>
2024-07-03 12:49:35 +0600

    Time: 68 ms (47.27%), Space: 51.9 MB (82.19%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.ts
+++ 3110-score-of-a-string/3110-score-of-a-string.ts
@@ -1,10 +1,7 @@
 function scoreOfString(s: string): number {
     let res = 0;
     for(let i = 0; i < s.length -1; i++) {
-        let first = s.charCodeAt(i);
-        let second = s.charCodeAt(i+1);
-        console.log(first);
-        res += Math.abs(first - second);
+        res += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1));
     }
     return res;
 };
