85b99be36e22e0341c71eddcb2d3a54a26948402
Mahmudul Hasan Autul <autul.aust@gmail.com>
2025-02-19 22:24:38 +0600

    Time: 2 ms (83.24%) | Memory: 61 MB (5.10%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.ts
@@ -0,0 +1,13 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let max = 0;
+    let count = 0;
+    for(let i = 0; i<nums.length; i++) {
+        if(nums[i] === 1) {
+            count++;
+            if(max<count) max = count;
+        } else {
+            count = 0;
+        }
+    }
+    return max;
+};
